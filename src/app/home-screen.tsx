0ba26a22507a928e1de1d40967bc2ccc3066909c
rafaelortet <rafael.ortet@84000.co>
2026-01-09 16:21:45 +0100

    Add home header section. Edit the H1, and tagline of the Header.

--- src/app/home-screen.tsx
+++ src/app/home-screen.tsx
@@ -1,77 +1,96 @@
 "use client";
 
-import { BookOpen01, Check, Copy01, Cube01, HelpCircle } from "@untitledui/icons";
+import type { ComponentProps } from "react";
+import { CreditCard } from "@/components/shared-assets/credit-card/credit-card";
+import { PlayCircle } from "@untitledui/icons";
 import { Button } from "@/components/base/buttons/button";
-import { ButtonUtility } from "@/components/base/buttons/button-utility";
-import { UntitledLogoMinimal } from "@/components/foundations/logo/untitledui-logo-minimal";
 import { useClipboard } from "@/hooks/use-clipboard";
+import { cx } from "@/utils/cx";
 
 export const HomeScreen = () => {
     const clipboard = useClipboard();
 
     return (
-        <div className="flex h-dvh flex-col">
-            <div className="flex min-h-0 flex-1 flex-col items-center justify-center px-4 md:px-8">
-                <div className="relative flex size-28 items-center justify-center">
-                    <UntitledLogoMinimal className="size-10" />
+        <div className="relative overflow-hidden bg-brand-section">
+            {/* Background pattern */}
+            <img
+                alt="Grid of dots"
+                aria-hidden="true"
+                loading="lazy"
+                src="https://www.untitledui.com/patterns/light/grid-dot-sm-desktop.svg"
+                className="pointer-events-none absolute top-0 left-1/2 z-0 hidden max-w-none -translate-x-1/2 opacity-20 md:block dark:opacity-100 dark:brightness-[0.2]"
+            />
+            <img
+                alt="Grid of dots"
+                aria-hidden="true"
+                loading="lazy"
+                src="https://www.untitledui.com/patterns/light/grid-dot-sm-mobile.svg"
+                className="pointer-events-none absolute top-0 left-1/2 z-0 max-w-none -translate-x-1/2 opacity-20 md:hidden dark:opacity-100 dark:brightness-[0.2]"
+            />
+
+            <section className="relative py-16 md:pb-24">
+                <img
+                    alt="Light Accent"
+                    aria-hidden="true"
+                    src="https://www.untitledui.com/marketing/light-accent.webp"
+                    className="absolute -right-4 -bottom-14 max-w-160 opacity-90 mix-blend-screen md:right-4 md:-bottom-1/3 md:max-w-7xl"
+                />
+
+                <div className="mx-auto grid max-w-container grid-cols-1 items-center gap-16 px-4 md:px-8 lg:grid-cols-2 lg:gap-16">
+                    <div className="flex max-w-3xl flex-col items-start">
+                        <span className="text-sm font-semibold text-secondary_on-brand md:text-md">Open. Inclusive. Relevant.</span>
+
+                        <h1 className="mt-3 text-display-md font-semibold text-primary_on-brand md:text-display-lg lg:text-display-xl">
+                            Gateways for Buddhist study and practice.
+                        </h1>
+                        <p className="mt-4 max-w-120 text-lg text-balance text-tertiary_on-brand md:mt-6 md:text-xl">
+                            A modern platform to channel universally revered Buddhist teachings for everyone.{" "}
+                        </p>
+
+                        <div className="mt-8 flex w-full flex-col-reverse items-stretch gap-3 md:mt-12 md:flex-row md:items-start">
+                            <Button color="secondary" size="xl" iconLeading={PlayCircle} className="shadow-xs! ring-0">
+                                Intro
+                            </Button>
+                            <Button size="xl">Sign up</Button>
+                        </div>
+                    </div>
+
+                    <div className="relative -mx-4 flex h-80 items-center justify-center md:mx-0 md:h-120 lg:h-full lg:min-h-140">
+                        <div className="translate-x-[34px] translate-y-[3px] -space-y-[116.5px] md:translate-x-[53px] md:translate-y-[37px] md:-space-y-[83px]">
+                            <div className="relative z-3 translate-y-[22px] rotate-[29.9deg]">
+                                <div
+                                    className="[--scale:1.365] md:[--scale:2.1]"
+                                    style={{
+                                        transform: "scale(var(--scale)) rotateX(63deg) rotateY(1deg) rotateZ(51deg) skewX(14deg)",
+                                    }}
+                                >
+                                    <CreditCard type="transparent-gradient" cardHolder="lana steiner" />
+                                </div>
+                            </div>
+                            <div className="relative z-2 translate-y-[10px] rotate-[14.8deg]">
+                                <div
+                                    className="[--scale:1.365] md:[--scale:2.099]"
+                                    style={{
+                                        transform: "scale(var(--scale)) rotateX(63deg) rotateY(1deg) rotateZ(51deg) skewX(14deg)",
+                                    }}
+                                >
+                                    <CreditCard type="transparent-gradient" cardHolder="OLIVIA RHYE" />
+                                </div>
+                            </div>
+                            <div
+                                className="relative z-1 [--scale:1.365] md:[--scale:2.1]"
+                                style={{
+                                    transform: "scale(var(--scale)) rotateX(63deg) rotateY(1deg) rotateZ(51deg) skewX(14deg)",
+                                }}
+                            >
+                                <CreditCard type="transparent-gradient" cardHolder="Phoenix Baker" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </section>
+        </div >
 
-                <h1 className="max-w-3xl text-center text-display-sm font-semibold text-primary">Welcome to Bodhi Central</h1>
 
-                <p className="mt-2 max-w-xl text-center text-lg text-tertiary">
-                    A public hub of dharma's teachings for awakening.
-                </p>
-
-                <div className="relative mt-6 flex h-10 items-center rounded-lg border border-secondary bg-secondary">
-                    <code className="px-3 font-mono text-secondary">npx untitledui@latest add</code>
-
-                    <hr className="h-10 w-px bg-border-secondary" />
-
-                    <ButtonUtility
-                        color="tertiary"
-                        size="sm"
-                        tooltip="Copy"
-                        className="mx-1"
-                        icon={clipboard.copied ? Check : Copy01}
-                        onClick={() => clipboard.copy("npx untitledui@latest add")}
-                    />
-                </div>
-
-                <div className="mt-6 flex items-center gap-3">
-                    <Button
-                        href="https://www.untitledui.com/react/docs/introduction"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={BookOpen01}
-                    >
-                        Docs
-                    </Button>
-                    <div className="h-px w-4 bg-brand-solid" />
-                    <Button
-                        href="https://www.untitledui.com/react/resources/icons"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={Cube01}
-                    >
-                        Icons
-                    </Button>
-                    <div className="h-px w-4 bg-brand-solid" />
-                    <Button
-                        href="https://github.com/untitleduico/react/issues"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={HelpCircle}
-                    >
-                        Help
-                    </Button>
-                </div>
-            </div>
-        </div>
     );
 };
