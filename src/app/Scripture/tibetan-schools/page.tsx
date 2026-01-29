9e570df6129fc10543f78783f1b5ea35a9d1aba8
rafaelortet <rafael.ortet@84000.co>
2026-01-30 00:17:43 +0100

    update the Kagyu school collection.

--- src/app/Scripture/tibetan-schools/page.tsx
+++ src/app/Scripture/tibetan-schools/page.tsx
@@ -21,7 +21,7 @@ export default function TibetanSchoolsPage() {
                                 <Image src="/collections_thumbnails_gelug.jpg" alt="Gelug" width={190} height={190} className="rounded-full drop-shadow-xl" />
                                 <div className="flex flex-col items-center justify-center bg-white/65 dark:bg-black/40 p-2 border-t-2 border-b-2 border-yellow-800/40 rounded-t-4xl rounded-b-4xl">
                                     <h2 className="text-display-sm text-primary text-center py-2">Gelug Studies</h2>
-                                    <p className="pb-2 text-md text-tertiary text-center">Standardized Tibetan studies into five genres: Pramana, Prajnaparamita, Madhyamika,
+                                    <p className="pb-2 text-md text-tertiary text-center">Standardized Tibetan studies into five genres: Pramana, Prajnaparamita, Madhyamaka,
                                         Abhidharma, and Vinaya.</p>
                                 </div>
                             </div>
@@ -43,7 +43,7 @@ export default function TibetanSchoolsPage() {
                                 <Image src="/collections_thumbnails_kagyu.jpg" alt="Kagyu" width={190} height={190} className="rounded-full drop-shadow-xl" />
                                 <div className="flex flex-col items-center justify-center bg-white/65 dark:bg-black/40 p-2 border-t-2 border-b-2 border-pink-800/40 rounded-t-4xl rounded-b-4xl">
                                     <h2 className="text-display-sm text-primary text-center py-2">Kagyu Studies</h2>
-                                    <p className="pb-2 text-md text-tertiary text-center">Eight Kagyu treatises on Pramana, Madhyamika, Prajnaparamita, Abhidharma, Vinaya, and Vajrayana.</p>
+                                    <p className="pb-2 text-md text-tertiary text-center">Eight Kagyu treatises on Pramana, Madhyamaka, Prajnaparamita, Abhidharma, Vinaya, and Vajrayana.</p>
                                 </div>
                             </div>
                         </div>
