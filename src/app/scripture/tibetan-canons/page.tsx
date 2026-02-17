878a1adf26acf64b3aca658df93d97f6aef6e4f3
rafaelortet <rafael.ortet@84000.co>
2026-02-17 23:16:38 +0100

    update gradients on Tibetan Canon Header cards.

--- src/app/scripture/tibetan-canons/page.tsx
+++ src/app/scripture/tibetan-canons/page.tsx
@@ -18,7 +18,7 @@ export default function TibetanCanonsPage() {
                     <section>
                         <div className="mx-auto max-w-4xl px-4 pb-20 md:px-6 grid grid-cols-2 md:grid-cols-2 gap-16 items-start">
                             <div className="flex flex-col items-center justify-center gap-0">
-                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 from-0% via-white-30 via-20% to-white/40 dark:bg-linear-to-b dark:from-black/0 dark:from-0% dark:via-black/30 dark:via-25% dark:to-black/40 p-4 rounded-[4rem]">
+                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-50% to-white/0 dark:bg-linear-to-b dark:from-black/0 dark:via-black/35 dark:via-50% dark:to-black/0 p-4 rounded-[6rem]">
                                     <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={120} height={120} className="drop-shadow-lg" />
                                     <h2 className="text-display-sm text-primary text-center py-2">The Kangyur</h2>
                                     <p className="pb-4 text-md font-semibold text-tertiary text-center text-balance">The Kangyur is the collection of the translated words of the Buddha in Tibetan.</p>
@@ -28,8 +28,8 @@ export default function TibetanCanonsPage() {
                                 </div>
                             </div>
                             <div className="flex flex-col items-center justify-center gap-0">
-                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 from-0% via-white-30 via-20% to-white/40 dark:bg-linear-to-b dark:from-black/0 dark:from-0% dark:via-black/30 dark:via-25% dark:to-black/40 p-4 rounded-[4rem]">
-                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={120} height={120} className="drop-shadow-lg " />
+                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-50% to-white/0 dark:bg-linear-to-b dark:from-black/0 dark:via-black/35 dark:via-50% dark:to-black/0 p-4 rounded-[6rem]">
+                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={120} height={120} className="drop-shadow-lg" />
                                     <h2 className="text-display-sm text-primary text-center py-2">The Tengyur</h2>
                                     <p className="pb-4 text-md font-semibold text-tertiary text-center text-balance">The Tengyur is the collection of the translated Buddhist treatises in Tibetan.</p>
                                     <Button color="secondary" size="sm">
