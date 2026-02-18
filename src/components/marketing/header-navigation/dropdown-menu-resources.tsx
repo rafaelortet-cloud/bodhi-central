caf4910d4792bbaeafdbd139a85f8de5e0d48909
rafaelortet <rafael.ortet@84000.co>
2026-02-18 21:18:13 +0100

    adjust the overflow-hidden property for the md resolution.

--- src/components/marketing/header-navigation/dropdown-menu-resources.tsx
+++ src/components/marketing/header-navigation/dropdown-menu-resources.tsx
@@ -70,7 +70,7 @@ const columns: MenuColumn[] = [
 export const DropdownMenuResources = () => {
     return (
         <div className="px-3 pb-2 md:max-w-200 md:p-0">
-            <nav className="overflow-hidden rounded-xl bg-brand-100 dark:bg-brand-900 drop-shadow-lg ring-1 ring-secondary_alt md:rounded-3xl md:drop-shadow-2xl dark:md:drop-shadow-gray-950">
+            <nav className="overflow-hidden md:overflow-visible rounded-xl bg-brand-100 dark:bg-brand-900 drop-shadow-lg ring-1 ring-secondary_alt md:rounded-3xl md:drop-shadow-2xl dark:md:drop-shadow-gray-950">
                 <div className="flex flex-col gap-5 rounded-xl bg-brand-25 dark:bg-brand-950 pt-4 pb-5 ring-1 ring-secondary md:gap-8 md:rounded-t-2xl md:p-6 md:pt-5">
                     <div className="flex flex-col items-center gap-1 px-4 md:p-0">
                         <h4 className="pt-4 text-4xl font-light">RESOURCES</h4>
