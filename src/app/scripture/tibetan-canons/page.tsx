383465a5d72059e5a24d36c9d36f8aef9bef6125
rafaelortet <rafael.ortet@84000.co>
2026-02-19 00:49:14 +0100

    hiddden the pecha images from the header section of the Tibetan canon landing page.

--- src/app/scripture/tibetan-canons/page.tsx
+++ src/app/scripture/tibetan-canons/page.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/base/buttons/button';
 export default function TibetanCanonsPage() {
     return (
         <>
-            <div className="relative mx-auto bg-[url(/tibetan-buddhist-library-01.jpg)] bg-cover bg-center bg-no-repeat pb-36">
+            <div className="relative mx-auto bg-[url(/tibetan-buddhist-library-01.jpg)] bg-position-bottom bg-cover bg-center bg-no-repeat pb-40">
                 {/* Overlay layer for easy customization of color/transparency */}
                 <div className="absolute inset-0 bg-white/55 dark:bg-black/55 z-0" aria-hidden="true" />
 
@@ -19,8 +19,8 @@ export default function TibetanCanonsPage() {
                     <section>
                         <div className="mx-auto max-w-4xl px-4 pb-20 md:px-6 grid grid-cols-2 md:grid-cols-2 gap-16 items-start">
                             <div className="flex flex-col items-center justify-center gap-0">
-                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/0 dark:via-black/55 dark:via-55% dark:to-black/0 p-4 rounded-[7rem]">
-                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={100} height={100} className="drop-shadow-lg" />
+                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/10 dark:via-black/55 dark:via-55% dark:to-black/10 p-4 rounded-[7rem]">
+                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={100} height={100} className="hidden drop-shadow-lg" />
                                     <h2 className="text-display-sm text-primary text-center py-2">The Kangyur</h2>
                                     <p className="pb-4 text-md font-semibold text-tertiary text-center text-balance">The Kangyur is the collection of the translated words of the Buddha in Tibetan.</p>
                                     <Button color="secondary" size="sm">
@@ -29,8 +29,8 @@ export default function TibetanCanonsPage() {
                                 </div>
                             </div>
                             <div className="flex flex-col items-center justify-center gap-0">
-                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/0 dark:via-black/55 dark:via-55% dark:to-black/0 p-4 rounded-[7rem]">
-                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={100} height={100} className="drop-shadow-lg" />
+                                <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/10 dark:via-black/55 dark:via-55% dark:to-black/10 p-4 rounded-[7rem]">
+                                    <Image src="/pecha-wrapped-on-cloth-600.webp" alt="Sakya" width={100} height={100} className="hidden drop-shadow-lg" />
                                     <h2 className="text-display-sm text-primary text-center py-2">The Tengyur</h2>
                                     <p className="pb-4 text-md font-semibold text-tertiary text-center text-balance">The Tengyur is the collection of the translated Buddhist treatises in Tibetan.</p>
                                     <Button color="secondary" size="sm">
