848fecd184df6ec00a5775e006e50eee37201cc5
rafaelortet <rafael.ortet@84000.co>
2026-02-24 20:33:01 +0100

    adjust ornament details.

--- src/app/scripture/theravada-tradition/page.tsx
+++ src/app/scripture/theravada-tradition/page.tsx
@@ -24,7 +24,7 @@ export default function TheravadaTraditionPage() {
                             <div className="flex flex-col items-center justify-center gap-0">
 
                                 <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/10 dark:via-black/55 dark:via-55% dark:to-black/10 p-4 rounded-[7rem]">
-                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-normal drop-shadow-sm drop-shadow-gray-700" />
+                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-multiply drop-shadow-xs drop-shadow-black/90 dark:drop-shadow-black/50 opacity-70" />
                                     <h2 className="text-display-sm text-primary text-center py-2">Vinaya Piṭaka</h2>
                                     <p className="text-md text-tertiary text-center pb-4">The first basket of the Pali Canon, containing the rules and regulations for monks and nuns.</p>
                                     <Button color="secondary" size="sm">
@@ -34,7 +34,7 @@ export default function TheravadaTraditionPage() {
                             </div>
                             <div className="flex flex-col items-center justify-center gap-0">
                                 <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/10 dark:via-black/55 dark:via-55% dark:to-black/10 p-4 rounded-[7rem]">
-                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-normal drop-shadow-sm drop-shadow-gray-700" />
+                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-multiply drop-shadow-xs drop-shadow-black/90 dark:drop-shadow-black/50 opacity-70" />
                                     <h2 className="text-display-sm text-primary text-center py-2">Sutta Piṭaka</h2>
                                     <p className="pb-4 text-md text-tertiary text-center">The second basket of the Pali Canon, containing the discourses of the Buddha.</p>
                                     <Button color="secondary" size="sm">
@@ -44,7 +44,7 @@ export default function TheravadaTraditionPage() {
                             </div>
                             <div className="flex flex-col items-center justify-center gap-0">
                                 <div className="flex flex-col items-center justify-center bg-linear-to-b from-white/0 via-white/35 via-55% to-white/0 dark:bg-linear-to-b dark:from-black/10 dark:via-black/55 dark:via-55% dark:to-black/10 p-4 rounded-[7rem]">
-                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-normal drop-shadow-sm drop-shadow-gray-700" />
+                                    <Image src="/thai-ornament-top-linear-logo.png" alt="thai ornament" width={100} height={100} className="mix-blend-multiply drop-shadow-xs drop-shadow-black/90 dark:drop-shadow-black/50 opacity-70" />
                                     <h2 className="text-display-sm text-primary text-center py-2">Abhidhamma Piṭaka</h2>
                                     <p className="pb-4 text-md text-tertiary text-center">The third basket of the Pali Canon, containing the higher teachings and philosophical analysis.</p>
                                     <Button color="secondary" size="sm">
